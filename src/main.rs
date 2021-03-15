1578cbf8954dd46658fd9859ff4372959720d3c6
= <=>
2021-03-15 09:41:09 +0300

    Replaced binary operiation checks with is_none() or is_some()

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,21 @@ fn main() {
     while !quit {
         println!("Multiplayer or Ai(M/A): ");
         let game_mode: char = read!();
-        if game_mode == 'M' || game_mode == 'm'{
+        if game_mode == 'M' || game_mode == 'm' {
             game.start_game();
-        }else if game_mode == 'A' || game_mode == 'a' {
+        } else if game_mode == 'A' || game_mode == 'a' {
             game.start_game_vs_ai();
         }
         println!("Do you want to quit?(Y/N)");
         let is_quiting: char = read!();
-        if is_quiting == 'Y' || is_quiting == 'y'{
+        if is_quiting == 'Y' || is_quiting == 'y' {
             quit = true;
         }
         game.clear_board();
     }
 }
 #[test]
-fn test_negamax(){
+fn test_negamax() {
     let mut game = Game::new();
     game.start_game_vs_ai();
 }
